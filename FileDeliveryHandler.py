53e76fcd36a6a8050194152b58d5479c4649d11b
nawabgit <nawab.mir14@gmail.com>
2019-07-15 01:02:57 +0100

    added MultipartEncoder wrapper for http requests

--- FileDeliveryHandler.py
+++ FileDeliveryHandler.py
@@ -1,4 +1,6 @@
 import requests
+from pathlib import Path
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 
 class FileDeliveryHandler:
@@ -6,8 +8,20 @@ class FileDeliveryHandler:
     def __init__(self, url='https://file.io'):
         self.post_url = url
 
-    def post_file_to_api(self, path_to_file):
-        file = {'file': (path_to_file, open(path_to_file, 'rb'))}
-        r = requests.post(self.post_url, files={'file': file})
+    def post_file_to_api(self, path):
+        """
+        Posts file to API using MultipartEncoder
+            so progress can be monitored
+
+            :param path path to file
+            :returns status JSON
+        """
+
+        file = open(path, 'rb')
+        name = Path(path).with_suffix('').stem
+
+        encoder = MultipartEncoder({'file': (name, file)})
+        r = requests.post(self.post_url, files={'file': encoder},
+                          headers={'Content-Type': encoder.content_type})
 
         return r.text
